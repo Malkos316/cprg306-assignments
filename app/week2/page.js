d6adfd353848111ba8dbfa198308b51f929a1441
Malkos316 <118082476+Malkos316@users.noreply.github.com>
2023-09-15 16:34:47 -0600

    Create page.js
    created week 2 page and linked student info to it

---
+++ app/week2/page.js
@@ -0,0 +1,15 @@
+import Link from 'next/link'
+import { studentInfo } from '../StudentInfo' 
+
+export default function Page() {
+  return (
+    <div>
+      <h1>
+        My Shopping List
+      </h1> 
+      <p>
+        {studentInfo()}
+      </p>
+    </div>
+      );
+    }
