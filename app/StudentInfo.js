721f2c276d85970d83973c5e6943dae9806c1007
Malkos316 <118082476+Malkos316@users.noreply.github.com>
2023-09-15 16:34:20 -0600

    Create StudentInfo.js
    created my student info page and added link to github

---
+++ app/StudentInfo.js
@@ -0,0 +1,10 @@
+import Link from 'next/link'
+export function studentInfo() {
+    return (
+        <>
+        <p>Student Name: Markus Luthi</p>
+        <p>Course section: CPRG 306 A</p>
+        <Link href="https://github.com/Malkos316/cprg306-assignments">Github Account</Link>
+        </>
+    );
+}
