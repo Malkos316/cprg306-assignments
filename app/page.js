7febf1b81bf4ad5fc1db572685777051e5fcea2c
Malkos316 <118082476+Malkos316@users.noreply.github.com>
2023-09-29 16:35:30 -0600

    Update page.js
    Add link from homepage to week 3 assignment.

--- app/page.js
+++ app/page.js
@@ -9,7 +9,12 @@ export default function Page() {
       <p>
         {studentInfo()}
       </p>
-      <Link href="/week2">Week 2</Link>
+      <p>
+        <Link href="/week2">Week 2</Link>
+      </p>
+      <p>
+        <Link href="/week3">Week 3</Link>
+      </p>
     </main>
   );
 }        
