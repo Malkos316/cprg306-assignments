2a5f01473635ea115d37dc81c7e8864d96556631
Malkos316 <118082476+Malkos316@users.noreply.github.com>
2023-09-29 08:30:02 -0600

    Update item.js
    Create Functional component and props

---
+++ app/week3/item.js
@@ -0,0 +1,9 @@
+export default function Item({name, quantity, category}) {
+    return (
+        <div>
+            <li>
+                {name} {quantity} {category}
+            </li>
+        </div>
+    )
+}
