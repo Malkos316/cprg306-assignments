5f649d02c0a5a12ba6d197071a697b6526035dbb
Malkos316 <118082476+Malkos316@users.noreply.github.com>
2023-09-29 17:04:35 -0600

    Update item.js
    label each component

--- app/week3/item.js
+++ app/week3/item.js
@@ -2,8 +2,15 @@ export default function Item({name, quantity, category}) {
     return (
         <div className="border-1 max-w-sm m-2 p-2">
             <li>
-                <h2 className="text-xl font-bold ">{name}</h2>
-                {quantity} {category}
+                <h2 className="text-xl font-bold ">
+                    {name}
+                </h2>
+                <p>
+                    Qty: {quantity}
+                </p>
+                 <p>
+                    Aisle: {category}
+                </p>
             </li>
         </div>
     );
