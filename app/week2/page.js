1454d9d527646dfc932c17d55f50f9ffddb1b60b
Malkos316 <118082476+Malkos316@users.noreply.github.com>
2023-09-29 16:58:21 -0600

    Update page.js
    Format div -> main
    add styling

--- app/week2/page.js
+++ app/week2/page.js
@@ -3,14 +3,14 @@ import { studentInfo } from '../StudentInfo'
 
 export default function Page() {
   return (
-    <div>
-      <h1>
+    <main className='p-10'>
+      <h1 className=" flex flex-col items-center text-4xl font-thick">
         My Shopping List
       </h1> 
       <p>
         {studentInfo()}
       </p>
-    </div>
+    </main>
       );
     }
     
