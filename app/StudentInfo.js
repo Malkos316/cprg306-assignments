693a99b006b1e4cf56fb9b269951187b4fa2915e
Malkos316 <118082476+Malkos316@users.noreply.github.com>
2023-09-29 16:51:14 -0600

    Update StudentInfo.js
    Formatting

--- app/StudentInfo.js
+++ app/StudentInfo.js
@@ -2,8 +2,12 @@ import Link from 'next/link'
 export function studentInfo() {
     return (
         <>
-        <p>Student Name: Markus Luthi</p>
-        <p>Course section: CPRG 306 A</p>
+        <p>
+            Student Name: Markus Luthi
+        </p>
+        <p>
+            Course section: CPRG 306 A
+        </p>
         <Link href="https://github.com/Malkos316/cprg306-assignments">Github Account</Link>
         </>
     );
