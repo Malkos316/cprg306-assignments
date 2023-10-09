41c8144d2b34c3245e257312baabcd81df4609f7
Malkos316 <118082476+Malkos316@users.noreply.github.com>
2023-10-09 12:17:59 -0600

    Update page.js
    Add return to week 4 page

--- app/week4/page.js
+++ app/week4/page.js
@@ -1,1 +1,12 @@
-import newItem from './new-item.js';
+import newItem from "./new-item";
+
+export default function Page() {
+    return (
+        <main>
+            <h1>
+                Shopping List
+            </h1>
+        <newItem  />
+        </main>
+    )
+    }
