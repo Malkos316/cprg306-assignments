f5408d2a891685094649fe45f44ecd3b7d0c402c
Malkos316 <118082476+Malkos316@users.noreply.github.com>
2023-10-09 14:17:08 -0600

    Update new-item.js
    Make base code without styling

--- app/week4/new-item.js
+++ app/week4/new-item.js
@@ -1,10 +1,12 @@
-"use client"
+"use client";
+
 import { useState } from "react";
 
-export default function newItem() {
+export default function NewItem() {
     const [name, setName] = useState("");
     const [quantity, setQuantity] = useState(1);
     const [category, setCategory] = useState("produce");
+    const [eventCreated, setItemCreated] = useState(false);
 
     const handleSubmit = (event) => {
         event.preventDefault();
@@ -17,8 +19,65 @@ export default function newItem() {
 
         console.log(newItem);
 
-        setEventCreated(true);
+        setItemCreated(true);
 
-        alert("Item created!");
-    }
+        setName("fake name");
+        setQuantity(1);
+        setCategory("produce");
+
+        setItemCreated(false);
+    };
+
+    const handleNameChange = (event) => {
+        setName(event.target.value);
+    };
+
+    const handleQuantityChange = (event) => {
+        setQuantity(event.target.value);
+    };
+
+    const handleCategoryChange = (event) => {
+        setCategory(event.target.value);
+    };
+
+    return (
+        <div>
+            <h2>Add an item</h2>
+            <form onSubmit={handleSubmit}>
+                <label>
+                    Name:
+                    <input className="text-black"
+                        type="text"
+                        value={name}
+                        onChange={handleNameChange}
+                    />
+                </label>
+                <label>
+                    Quantity:
+                    <input className="text-black"
+                        type="number"
+                        value={quantity}
+                        onChange={handleQuantityChange}
+                    />
+                </label>
+                <label>
+                    Category:
+                    <select className="text-black"
+                        value={category}
+                        onChange={handleCategoryChange}
+                    >
+                        <option value="produce">Produce</option>
+                        <option value="dairy">Dairy</option>
+                        <option value="bakery">Bakery</option>
+                        <option value="meat">Meat</option>
+                        <option value="canned goods">Canned Goods</option>
+                        <option value="dry goods">Dry Goods</option>
+                        <option value="household">Household</option>
+                    </select>
+                </label>
+                <button type="submit">Add</button>
+            </form>
+            {eventCreated && <p>Item created!</p>}
+        </div>
+    );
 }
