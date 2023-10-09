1bcd22e680c3470799f1b6ff150a6dcccae9f28a
Malkos316 <118082476+Malkos316@users.noreply.github.com>
2023-10-09 12:16:20 -0600

    Update page.js
    add week 5demo to main page

--- app/page.js
+++ app/page.js
@@ -18,6 +18,9 @@ export default function Page() {
       <p>
         <Link href="/week4">Week 4</Link>
       </p>
+      <p>
+        <Link href="/week5Demo">Week 5</Link>
+      </p>
     </main>
   );
 }        
