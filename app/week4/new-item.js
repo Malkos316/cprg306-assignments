7a5ad1638fee43efca829af9a48b87e16b2d4e98
Malkos316 <118082476+Malkos316@users.noreply.github.com>
2023-10-09 19:12:07 -0600

    Update new-item.js
    Fix formatting so its easier to read. add onChange functionality

--- app/week4/new-item.js
+++ app/week4/new-item.js
@@ -44,11 +44,25 @@ export default function NewItem() {
         <main className="flex justify-center">
             <form  className=" text-black bg-slate-700 p-4 m-4 max-w-sm w-full rounded-lg">
                 <div className="mb-2">
-                    <input className=" p-2 rounded-lg w-full" type="text" placeholder="Item Name" value={setName} required=""/>
+                    <input className=" p-2 rounded-lg w-full" 
+                    type="text" 
+                    placeholder="Item Name" 
+                    value={name} 
+                    required=""
+                    onChange={handleNameChange}/>
                 </div>
                 <div className="mb-4 flex justify-between">
-                    <input className="w-20 p-2 rounded-lg" type="number" min={1} max={99} required=""/>
-                    <select className="rounded-lg p-2" value={category} required="">
+                    <input className="w-20 p-2 rounded-lg" 
+                    type="number"
+                     min={1} 
+                     max={99} 
+                     value={quantity}
+                     required=""
+                     onChange={handleQuantityChange}/>
+                    <select className="rounded-lg p-2" 
+                    value={category} 
+                    required=""
+                    onChange={handleCategoryChange}>
                         <option value="produce">Produce</option>
                         <option value="dairy">Dairy</option>
                         <option value="meat">Meat</option>
@@ -59,7 +73,8 @@ export default function NewItem() {
                     </select>
                 </div>
                 <div>
-                    <button className="bg-white rounded-lg w-full p-2 hover:bg-amber-500" type="submit">Add to List</button>
+                    <button className="bg-white rounded-lg w-full p-2 hover:bg-amber-500" 
+                    type="submit">Add to List</button>
                 </div>
             </form>
         </main>
