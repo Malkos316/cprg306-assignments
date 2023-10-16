dc5cf4338087013d51fcfa5b874b331795ec9460
Malkos316 <118082476+Malkos316@users.noreply.github.com>
2023-10-15 21:55:50 -0600

    Update page.js
    Add hover styling to main page

--- app/page.js
+++ app/page.js
@@ -2,26 +2,29 @@ import { studentInfo } from "./StudentInfo"
 import Link from 'next/link'
 export default function Page() {
   return (
-    <main className=" p-10">
-      <h1 className=" flex flex-col items-center text-3xl">
+    <main className=" flex flex-col items-center text-center" >
+      <h1 className=" flex flex-col items-center text-3xl p-10 bg-gradient-to-r from-black to-gray-600">
         CPRG 306:Web Development 2 - assignments
       </h1>
       <p>
         {studentInfo()}
       </p>
-      <p>
-        <Link href="/week2">Week 2</Link>
+      <p className="m-1 hover:text-cyan-400" >
+        <Link href="/week2">Week 2 ✔</Link>
       </p>
-      <p>
-        <Link href="/week3">Week 3</Link>
+      <p className="m-1 hover:text-cyan-400" >
+        <Link href="/week3">Week 3 ✔</Link>
       </p>
-      <p>
-        <Link href="/week4">Week 4</Link>
+      <p className="m-1 hover:text-cyan-400" >
+        <Link href="/week4">Week 4 ✔</Link>
       </p>
-      <p>
-        <Link href="/week5">Week 5</Link>
+      <p className="m-1 hover:text-cyan-400" >
+        <Link href="/week5">Week 5 ✔</Link>
       </p>
-      <p>
+      <p className="m-1 hover:text-cyan-400" >
+        <Link href="/week6">Week 6 🚧</Link>
+      </p>
+      <p className="m-1 hover:text-cyan-400" >
         <Link href="/week5Demo">Week 5 Demo</Link>
       </p>
     </main>
