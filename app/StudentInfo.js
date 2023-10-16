07d803b1ec71edcb709737d1e39550b86f8420b4
Malkos316 <118082476+Malkos316@users.noreply.github.com>
2023-10-15 21:55:18 -0600

    Update StudentInfo.js
    Adjust styling

--- app/StudentInfo.js
+++ app/StudentInfo.js
@@ -8,7 +8,7 @@ export function studentInfo() {
         <p>
             Course section: CPRG 306 A
         </p>
-        <Link href="https://github.com/Malkos316/cprg306-assignments">Github Account</Link>
+        <Link className="m-1 hover:text-cyan-400" href="https://github.com/Malkos316/cprg306-assignments">Github Account</Link>
         </>
     );
 }
