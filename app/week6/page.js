97108c90cdcdda39c6b5a7c3c3c90519a2a3f4db
Malkos316 <118082476+Malkos316@users.noreply.github.com>
2023-10-23 21:34:53 -0600

    Update page.js
    Set up page as parent for use state information.

--- app/week6/page.js
+++ app/week6/page.js
@@ -1,12 +1,31 @@
+"use client";
+
 import ItemList from "./item-list";
+import NewItem from "./new-item";
+import itemsData from "./items.json";
+import { useState } from "react";
+
 
 export default function Page() {
-    return (
-      <main className="p-10">
-        <h1 className=" flex flex-col items-center text-4xl font-thick">
-          Shopping List
-        </h1>
-        <ItemList />
-      </main>
-    )
-      }
+
+	const [items, setItems] = useState(itemsData);
+
+	function handleAddItem(newItem) {
+		setItems([...items, newItem]);
+	}
+
+	return (
+		<main className="p-10">
+			<h1 className=" flex flex-col items-center text-4xl font-thick">
+				Shopping List
+			</h1>
+
+			<ItemList items={items} />
+
+			<NewItem onAddItem={handleAddItem} />
+
+		</main>
+	);
+}
+
+
